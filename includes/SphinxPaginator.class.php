ce1306b93f7b092a0cc4f07219596c57c852c8a0
Cameron Seright <seright@uoregon.edu>
2021-03-16 20:21:18 -0700

    Added an optional parameter to the functions which build links in the search results pages to determine if sphinx results links should be returned, then added the parameter to the function calls in the sphinx results page; removed the limit in the constructor of the paginator class; added a paginator class specific to the sphinx results page which looks up the total number of results in the constructor differently than the other paginator and outpus links that target the sphinx results page;

--- includes/SphinxPaginator.class.php
+++ includes/SphinxPaginator.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class Paginator {
+class SphinxPaginator {
 
      private $_conn;
      private $_limit;
@@ -8,16 +8,22 @@ class Paginator {
      private $_query;
      private $_total;
 
-
-
-
 public function __construct( $conn, $query ) {
 
     $this->_conn = $conn;
     $this->_query = $query;
 
-    $rs= $this->_conn->query( $this->_query );
-    $this->_total = $rs->num_rows;
+    $this->_conn->query( $this->_query ); // Execute the query.
+    // To find the total number of results with sphinx, a follow up query
+    // should be done to look up the previous query's meta info.
+    $rs = $this->_conn->query( 'SHOW META;' );
+    // Search through the rows and find the total_found variable.
+    while ( $row = $rs->fetch_assoc() ) {
+      if ($row['Variable_name'] == 'total_found') {
+        $this->_total = $row['Value'];
+        break;
+      }
+    }
 
 }
 
@@ -85,27 +91,27 @@ public function createLinks( $links, $list_class ) {
     $html       = '<ul class="' . $list_class . '">';
 
     $class      = ( $this->_page == 1 ) ? "disabled" : "";
-    $p_link     = ( $this->_page == 1 ) ? 'Previous <span class="show-for-sr">page</span>' : '<a href="results.php?' . $get_params . '&limit=' . $this->_limit . '&p=' . ( $this->_page - 1 ) . '" aria-label="Previous page">Previous <span class="show-for-sr">page</span></a>';
+    $p_link     = ( $this->_page == 1 ) ? 'Previous <span class="show-for-sr">page</span>' : '<a href="sphinx-results.php?' . $get_params . '&limit=' . $this->_limit . '&p=' . ( $this->_page - 1 ) . '" aria-label="Previous page">Previous <span class="show-for-sr">page</span></a>';
     $html       .= '<li class="pagination-previous ' . $class . '">' . $p_link . '</li>';
 
     if ( $start > 1 ) {
-        $html   .= '<li><a href="results.php?' . $get_params . '&limit=' . $this->_limit . '&p=1" aria-label="Page 1">1</a></li>';
+        $html   .= '<li><a href="sphinx-results.php?' . $get_params . '&limit=' . $this->_limit . '&p=1" aria-label="Page 1">1</a></li>';
         $html   .= '<li class="ellipsis" aria-hidden="true"></li>';
     }
 
     for ( $i = $start ; $i <= $end; $i++ ) {
         $class  = ( $this->_page == $i ) ? "current" : "";
-        $c_link = ( $this->_page == $i ) ? '<span class="show-for-sr">You\'re on page</span> ' . $i : '<a href="results.php?' . $get_params . '&limit=' . $this->_limit . '&p=' . $i . '" aria-label="' . $i . '">' . $i . '</a>';
+        $c_link = ( $this->_page == $i ) ? '<span class="show-for-sr">You\'re on page</span> ' . $i : '<a href="sphinx-results.php?' . $get_params . '&limit=' . $this->_limit . '&p=' . $i . '" aria-label="' . $i . '">' . $i . '</a>';
         $html   .= '<li class="' . $class . '">' . $c_link . '</li>';
     }
 
     if ( $end < $last ) {
         $html   .= '<li class="ellipsis" aria-hidden="true"></li>';
-        $html   .= '<li><a href="results.php?' . $get_params . '&limit=' . $this->_limit . '&p=' . $last . '" aria-label="Page ' . $last .'">' . $last . '</a></li>';
+        $html   .= '<li><a href="sphinx-results.php?' . $get_params . '&limit=' . $this->_limit . '&p=' . $last . '" aria-label="Page ' . $last .'">' . $last . '</a></li>';
     }
 
     $class      = ( $this->_page == $last ) ? "disabled" : "";
-    $n_link     = ( $this->_page == $last ) ? 'Next <span class="show-for-sr">page</span>' : '<a href="results.php?' . $get_params . '&limit=' . $this->_limit . '&p=' . ( $this->_page + 1 ) . '" aria-label="Next page">Next <span class="show-for-sr">page</span></a>';
+    $n_link     = ( $this->_page == $last ) ? 'Next <span class="show-for-sr">page</span>' : '<a href="sphinx-results.php?' . $get_params . '&limit=' . $this->_limit . '&p=' . ( $this->_page + 1 ) . '" aria-label="Next page">Next <span class="show-for-sr">page</span></a>';
     $html       .= '<li class="pagination-next ' . $class . '">' . $n_link . '</li>';
 
     $html       .= '</ul>';
