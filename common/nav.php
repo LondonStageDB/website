bd50fbf57f33dc035320e71ef367218ab6f114eb
Cameron Seright <seright@uoregon.edu>
2021-04-15 10:10:29 -0700

    Updated the navigation links to help with the UAT testing clarity; commented out the advanced search link on the Sphinx home search page;

--- common/nav.php
+++ common/nav.php
@@ -12,7 +12,7 @@
           <ul class="dropdown menu" data-dropdown-menu>
             <li><a href="/">Home</a></li>
             <li><a href="/sphinx-search-home.php">Faster Search Home</a></li>
-            <li><a href="/search.php">Search</a></li>
+            <li><a href="/search.php">Advanced Search (Legacy)</a></li>
             <li><a href="/data.php">Data</a></li>
             <li><a href="/guide.php">User Guide</a></li>
             <li><a href="/about.php">About</a></li>
