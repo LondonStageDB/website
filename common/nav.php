67952ead6d2e533a55708aa186c867ad6c1670bd
mipflug <76830897+mipflug@users.noreply.github.com>
2025-03-18 15:25:33 -0700

    nav-news-update

--- common/nav.php
+++ common/nav.php
@@ -1,43 +1,44 @@
 <div class="header">
     <nav>
-      <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-        <button class="menu-icon" type="button" data-toggle="example-menu"></button>
-        <div class="title-bar-title">Menu</div>
-      </div>
-      <div class="top-bar" id="example-menu">
-        <div class="top-bar-left">
-            <a href="/" class="menu-text">London Stage Database</a>
+        <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+            <button class="menu-icon" type="button" data-toggle="example-menu"></button>
+            <div class="title-bar-title">Menu</div>
         </div>
-        <div class="top-bar-right">
-  <ul class="dropdown menu" data-dropdown-menu>
-    <li><a href="/">Home</a></li>
-    <li><a href="/search.php">Advanced Search</a></li>
-    <li><a href="/legacy-search.php">Legacy Search</a></li>
-    <li><a href="/data.php">Full Data Sets</a></li>
-    <li>
-      <a href="/guide.php">User Guide</a>
-      <ul class="menu vertical">
-        <li><a href="/about-data.php">About the Data</a></li>
-        <li><a href="/glossary.php">Glossary</a></li>
-        <li><a href="/cast-list.php">Cast Lists</a></li>
-        <li><a href="/authors.php">Authors</a></li>
-        <li><a href="/dates.php">Dates</a></li>
-        <li><a href="/tips.php">Search Tips</a></li>
-      </ul>
-    </li>
-    <li>
-      <a href="about.php">About</a>
-      <ul class="menu vertical">
-        <li><a href="/history.php">History</a></li>
-        <li><a href="/team.php">Team</a></li>
-        <li><a href="/citation.php">Citation and Sharing</a></li>
-        <li><a href="/media-coverage.php">Media Coverage</a></li>
-      </ul>
-    </li>
-    <li><a href="/contact.php">Contact</a></li>
-  </ul>
-</div>
+        <div class="top-bar" id="example-menu">
+            <div class="top-bar-left">
+                <a href="/" class="menu-text">London Stage Database</a>
+            </div>
+            <div class="top-bar-right">
+                <ul class="dropdown menu" data-dropdown-menu>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/search.php">Advanced Search</a></li>
+                    <li><a href="/legacy-search.php">Legacy Search</a></li>
+                    <li><a href="/data.php">Full Data Sets</a></li>
+                    <li>
+                        <a href="/guide.php">User Guide</a>
+                        <ul class="menu vertical">
+                            <li><a href="/about-data.php">About the Data</a></li>
+                            <li><a href="/glossary.php">Glossary</a></li>
+                            <li><a href="/cast-list.php">Cast Lists</a></li>
+                            <li><a href="/authors.php">Authors</a></li>
+                            <li><a href="/dates.php">Dates</a></li>
+                            <li><a href="/tips.php">Search Tips</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="about.php">About</a>
+                        <ul class="menu vertical">
+                            <li><a href="/history.php">History</a></li>
+                            <li><a href="/team.php">Team</a></li>
+                            <li><a href="/citation.php">Citation and Sharing</a></li>
+                            <li><a href="/media-coverage.php">Media Coverage</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="/contact.php">Contact</a></li>
+                    <li><a href="news.php">News</a></li>
+                </ul>
+            </div>
 
-      </div>
+        </div>
     </nav>
 </div>
