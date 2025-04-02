022c9f07c62cc5bb0ae6e292edfff05b3d9cb041
mipflug <76830897+mipflug@users.noreply.github.com>
2025-04-02 13:14:11 -0700

    Content Reorganization and RSS Feed
    * content-reorganization
    
    * update history and team pages
    
    * dropdown changes
    
    * content reorg update
    
    * adds RSS feed from uo-zz
    
    * content reorg
    
    * nav-bar and updated CSS
    
    * navbar
    
    * reorg
    
    * Content-reorg
    
    * Added RSS feed.
    
    * coverage and dropdown formatting
    
    * indentation-fix
    
    * indent-fix
    
    * group-changes
    
    * image-additions
    
    * Update history.php
    
    * team-paths-fix
    
    * div-fixes
    
    * Completed the feed embedding, styling and words limit.
    
    * Create news.php
    
    * nav-news-update
    
    * Create rss.php
    
    * updated path
    
    * pr-comments-fixes
    
    * remove rss before merge
    
    * author-image-update
    
    ---------
    
    Co-authored-by: John <zzhao@uoregon.edu>
    Co-authored-by: Erin Winter <emwin@uoregon.edu>

---
+++ cast-list.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <?php include_once('common/header.php'); ?>
+    <title>Cast Lists London Stage Database</title>
+</head>
+
+<body id="cast-list">
+<?php include_once('common/nav.php'); ?>
+<div id="main" class="main grid-container">
+    <div class="grid-x cast-list-wrap">
+        <div class="small-12 page-heading">
+            <h1>Cast Lists</h1>
+        </div>
+        <div method="post" class="small-12 medium-11 large-9 cell grid-x cast-list-form">
+            <p>For the sake of concision, the editors of <i>The London Stage,
+                    1660-1800</i> used what they called a
+                "ladder" system to abbreviate cast lists, which were often the lengthiest part of a performance entry.
+                If the cast listed in the playbills and newspaper advertisements for a particular play stays fairly
+                consistent over the course of a season, it is reproduced in full the first time it appears with that
+                cast, and subsequent performances of the same play by the same company record the cast following the
+                syntax "As [date], but [substitutions, if any]." </p>
+            <p><img alt=""
+                    style="float:right;"
+                    src="/images/cast-list-ladder.png"><br/><br/>So, for
+                example, a performance of <i>The Busy
+                    Body</i>
+                at Drury Lane on 31 May 1739 is recorded "THE BUSY BODY. As 28 Dec. 1738, but Whisper-Winstone." The
+                user of the reference book can then turn to the entry for <i>The Busy Body</i> at Drury Lane on 28
+                December 1738 and read the cast list for that date: "As 23 Sept., but Marplot-Macklin;
+                Whisper-Woodward." Turning back to September, the reader finds a full cast list: "Marplot-Cibber; Sir
+                George-Mills; Sir Francis-Griffin; Miranda-Mrs Clive; Sir Jealous-Turbutt; Charles-Wright;
+                Whisper-Macklin; Isabinda-Mrs Mills; Patch-Mrs Pritchard; Scentwell-Mrs Bennet."</p>
+            <p>Making the appropriate
+                substitutions, the reader can infer that the cast list advertised for the December performance was "<b>Marplot-Macklin</b>;
+                Sir George-Mills; Sir Francis-Griffin; Miranda-Mrs Clive; Sir Jealous-Turbutt; Charles-Wright; <b>Whisper-Woodward</b>;
+                Isabinda-Mrs Mills; Patch-Mrs Pritchard; Scentwell-Mrs Bennet," and that for the May performance it was
+                "Marplot-Macklin; Sir George - Mills; Sir Francis - Griffin; Miranda - Mrs Clive; Sir Jealous - Turbutt;
+                Charles - Wright; <b>Whisper-Winstone</b>; Isabinda - Mrs Mills; Patch - Mrs Pritchard; Scentwell - Mrs
+                Bennet." Tracing these ladder cast lists without the aid of a database can be painstaking work if
+                several substitutions are made over a series of performances.
+            </p>
+            <p>The <i>London Stage Database</i> attempts to reconstruct the cast list wherever the "as [date]" syntax
+                occurs for a <a href='/glossary.php#mainpiece'>mainpiece</a> or <a href='/glossary.php#afterpiece'>afterpiece</a>.
+                However, it ignores entries that follow other patterns. For example, the editors sometimes provide notes
+                about an advertised cast list (or the lack thereof) and direct readers to "see [date]." This is an
+                invitation to comparison; it represents the editors' conjecture that these two dates' cast lists bear
+                some kind of relation. It does not, however, consistently represent a correspondence between advertised
+                casts in the same way that the "as [date]" syntax does, so we have chosen not to attempt cast lists for
+                these "see [date]" entries.
+            </p>
+            <p>Note that the program that reconstructs "as [date]" cast lists ignores the roles of <a
+                        href='/glossary.php#prologue-epilogue'>prologue</a> and <a
+                        href='/glossary.php#prologue-epilogue'>epilogue</a>. Because these short pieces were often
+                occasional and might only be spoken at initial performances of a play (or as reflections on special
+                events, like an author or actor <a href='/glossary.php#benefit'>benefit</a>), they are not assumed to
+                carry forward through subsequent performances in the same way that other roles do.
+            </p>
+        </div>
+    </div>
+</div>
+<?php include_once('common/footer.php'); ?>
+</body>
+
---
+++ citation.php
@@ -0,0 +1,189 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <?php include_once('common/header.php'); ?>
+    <title>Citing the London Stage Database</title>
+</head>
+
+<body id="citation">
+<?php include_once('common/nav.php'); ?>
+<div id="main" class="main grid-container">
+    <div class="grid-x">
+        <div class="small-12 page-heading">
+            <h1>Citation and Sharing</h1>
+        </div>
+        <div class="small-12 medium-4 large-3 citation-nav" id="citationNav">
+            <nav class="show-for-small-only citation-mobile-nav sticky-container" id="mobileNav" data-sticky-container>
+                <div class data-sticky data-anchor="mobileNav" data-sticky-on="small">
+                    <ul class="menu" data-magellan>
+                        <li><a href="#OpenResearch">Open Research</a></li>
+                        <li><a href="#Citing">Citing the Database</a></li>
+                        <li><a href="#ImageCredits">Image Credits</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <nav class="sticky-container show-for-medium" data-sticky-container>
+                <div class data-sticky data-anchor="citationNav" data-sticky-on="medium">
+                    <h2>On This Page</h2>
+                    <ul class="vertical menu" data-magellan>
+                        <li><a href="#OpenResearch">Open Research</a></li>
+                        <li><a href="#Citing">Citing the Database</a></li>
+                        <li><a href="#ImageCredits">Image Credits</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div id="top" class="small-12 medium-8 large-9 citation-content">
+            <div class="grid-x citation-section">
+                <div class="small-12">
+                    <p>Except where otherwise noted, content on this site is licensed under a <a
+                                href="https://creativecommons.org/licenses/by-nc/4.0/">Creative Commons
+                            Attribution-NonCommercial 4.0 International license</a>. You are welcome to copy,
+                        redistribute,
+                        remix, and build upon the material for non-commercial purposes, as long as you provide
+                        attribution, link to the license, and indicate the nature of your changes (if any) to the source
+                        material.</p>
+                    <p>Read on for more information about our open science commitments and practices, as well as how to
+                        access, reuse, and cite this work.</p>
+                </div>
+            </div>
+            <div class="grid-x citation-section">
+                <div id="OpenResearch" class="small-12" data-magellan-target="OpenResearch">
+                    <h2>Open Research</h2>
+                    <p>Our project is built on the principles and values of open research:</p>
+                    <ul>
+                        <li><b>Open data:</b> We embrace the <a href="http://go-fair.org" title="smartCard-inline">FAIR
+                                data principles</a> to promote the Findability, Accessibility, Interoperability, and
+                            Reusability of our data, and we reuse data from open sources wherever possible.
+                        </li>
+                        <li><b>Open file formats:</b> We use portable, open-source, non-proprietary file formats like
+                            XML, JPG, and PDF/A for preservation-friendly file management, following the latest
+                            recommendations of the Library of Congress. Our full datasets are available for export in
+                            SQL, JSON, XML, and CSV formats, encouraging interoperation with datasets in a range of
+                            other formats.
+                        </li>
+                        <li><b>Open source software:</b> Our website and database code are written in standard,
+                            open-source programming languages including PHP, Python, HTML, CSS, and JavaScript. The code
+                            base is available in full on <a href="https://github.com/LondonStageDB" title="‌">our GitHub
+                                site</a> and major release versions are automatically registered with unique DOIs
+                            through
+                            Zenodo:<br>
+                            <ul>
+                                <ul>
+                                    <li><a href="https://doi.org/10.5281/zenodo.7328444"><img
+                                                    src="https://zenodo.org/badge/140609672.svg" alt="DOI"></a> Data
+                                        v1.0
+                                    </li>
+                                    <li><a href="https://doi.org/10.5281/zenodo.7328442"><img
+                                                    src="https://zenodo.org/badge/140609563.svg" alt="DOI"></a>
+                                        Database Code v1.0
+                                    </li>
+                                    <li><a href="https://doi.org/10.5281/zenodo.7328435"><img
+                                                    src="https://zenodo.org/badge/140609156.svg" alt="DOI"></a>
+                                        Website v2.1
+                                    </li>
+                                </ul>
+                            </ul>
+                        </li>
+                        <li><b>Open access:</b> Consistent with the <a href="https://www.neh.gov/publicaccess"
+                                                                       title="‌">NEH public access policy</a>, we aim to
+                            publish project-related scholarly work in open-access venues whenever possible. A list of
+                            outputs and media coverage related to the first grant-funded phase of the project (2018-19)
+                            is maintained on the <a
+                                    href="https://apps.neh.gov/publicquery/AwardDetail.aspx?gn=HAA-258717-18" title="‌">NEH
+                                public query site</a>, and it will be joined by a running list of <a
+                                    href="https://apps.neh.gov/publicquery/AwardDetail.aspx?gn=HAA-300527-24" title="‌">associated
+                                products for the current grant</a> as they become available.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="grid-x citation-section">
+                <div id="Citing" class="small-12" data-magellan-target="Citing">
+                    <h2>Citing the Database</h2>
+                    <p>
+                        We credit and link back to our sources and greatly appreciate your help tracking our project’s
+                        impact through citations. A suggested citation, formatted in MLA and Chicago style, can be found
+                        at the bottom of each event page and follows this template:</p>
+                    <ul>
+                        <li>Chicago: "London Stage Event: 10 August 1682 at Dorset Garden Theatre." London Stage
+                            Database. Accessed March 10, 2025.
+                            https://londonstagedatabase.uoregon.edu/event.php?id=1494.
+                        <li>MLA: "London Stage Event: 10 August 1682 at Dorset Garden Theatre." London Stage Database,
+                            https://londonstagedatabase.uoregon.edu/event.php?id=1494.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="grid-x citation-section">
+                <div id="ImageCredits" class="small-12" data-magellan-target="ImageCredits">
+                    <h2>Image Credits</h2>
+                    <p>The image collage that is displayed on the landing page and in the background of the search,
+                        results,
+                        and events pages was created by Dustin Olson, and is covered by a <a
+                                href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC-BY-NC-SA 4.0 license</a>.
+                    </p>
+                    <p>Clockwise from top left:</p>
+                    <ul>
+                        <li><a href="https://www.britishmuseum.org/collection/object/P_Ee-4-80">“Scene from <i>School
+                                    for Scandal</i> being performed in Drury Lane Theatre, London,”</a> 1778, © The
+                            Trustees of the British Museum, CC-BY-NC-SA 4.0 license
+                        </li>
+                        <li><a href="https://collections.britishart.yale.edu/vufind/Record/3653228">William Blake, “<i>Beggar’s
+                                    Opera</i>, Act III,”</a> c.1790, Yale Center for British Art, Paul Mellon
+                            Collection, Public Domain
+                        </li>
+                        <li><a href="https://collections.britishart.yale.edu/vufind/Record/1670672">Thomas Rowlandson,
+                                “An Audience Watching a Play at Drury Lane Theatre,”</a> c.1785, Yale Center for British
+                            Art, Paul Mellon Collection, Public Domain
+                        </li>
+                        <li><a href="https://www.digitens.org/en/media/341"> Playbill from Burney Collection,
+                                “‘Theatrical Register.’ A collection of playbills of London theatres, chiefly of Drury
+                                Lane, Covent Garden and the Haymarket,”</a> 1774-1777, British Library, Public Domain
+                        </li>
+                        <li><a href="https://digitalcollections.folger.edu/img21902">John Lodge, “Mr. Garrick delivering
+                                his Ode at Drury Lane Theatre on dedicating a building & erecting a statue to
+                                Shakespeare,”</a> Folger Shakespeare Library, CC-BY-SA 4.0 license
+                        </li>
+                        <li>
+                            <a href="https://www.britishmuseum.org/research/collection_online/collection_object_details/collection_image_gallery.aspx?assetId=1613044433&objectId=3411178&partId=1">“Riot
+                                at Covent Garden Theatre, in 1763, in consequence of the Managers refusing to admit
+                                half-price in the Opera of <i>Artaxerxes</i>,”</a> 1763, © The Trustees of the British
+                            Museum, CC-BY-NC-SA 4.0 License
+                        </li>
+                        <li><a href="https://collections.britishart.yale.edu/catalog/tms:51853">Peter Lely, “Aphra
+                                Behn,”</a> 1670, Yale Center for British Art, Public Domain
+                        </li>
+                        <li>
+                            <a href="https://www.bl.uk/collection-items/third-edition-of-the-beggars-opera-by-john-gay-1729">John
+                                Gay, <i>The beggar's opera. As it is acted at the Theatre-Royal in Lincolns-Inn
+                                    Fields</i>,</a>1729, British Library, Public Domain
+                        </li>
+                        <li><a href="https://collections.britishart.yale.edu/catalog/tms:40620">Edward Fisher, “Miss
+                                Farren in the Character of Hermione,”</a> 1781, Yale Center for British Art, Paul Mellon
+                            Collection, Public Domain
+                        </li>
+                        <li>
+                            <a href="https://www.britishmuseum.org/research/collection_online/collection_object_details.aspx?assetId=290264001&objectId=752512&partId=1">“Scene
+                                One of <i>The Necromancer or Harlequin Dr Faustus</i> which opened at Lincoln's Inn
+                                Fields 20 December 1723,”</a> 1724, © The Trustees of the British Museum, CC-BY-NC-SA
+                            4.0 license
+                        </li>
+                        <li>
+                            <a href="https://www.britishmuseum.org/research/collection_online/collection_object_details.aspx?objectId=3553320&partId=1&searchText=theatre+ticket&images=true&from=ad&fromDate=1660&to=ad&toDate=1800&page=3">Thomas
+                                Bewick, “Border for theatre-ticket; concert for the benefit of Mr Evans, at the Theatre
+                                Royal in Haymarket, London, on 11 April 1777,”</a> 1777, © The Trustees of the British
+                            Museum, CC-BY-NC-SA 4.0 license
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include_once('common/footer.php'); ?>
+</body>
+
+</html>
