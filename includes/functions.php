8de8692285c6b545a894b0d6cf26b17f86fcd5c4
Erin Winter <emwin@uoregon.edu>
2025-08-15 14:54:18 -0700

    workid event interactions

--- includes/functions.php
+++ includes/functions.php
@@ -1053,7 +1053,7 @@
         $sources = array_filter(array_unique($sources)); // Deduplicate sources
     }
 
-    if ($perfTitle !== '') {
+    else if ($perfTitle !== '') {
       $prefix = "or ";
       $stopwords = ['[c|C]oncert[s]?', '[e|E]ntertainment[s]?'];
       $perfTitle =  preg_replace('/\b(' . implode('|', $stopwords) . ')\b/', '', $perfTitle);
