c5b56ae448f7a6f7896e4f53c77550ca0f4dc1dc
cmcc-oruo <ceilidh@uoregon.edu>
2026-03-13 13:03:42 -0700

    Revert "update to nav menu"
    This reverts commit 4be14fe07427aff8eb86eae2e6a02c965032278b.

--- common/nav.php
+++ common/nav.php
@@ -1,45 +1,40 @@
 <div class="header">
-    <nav aria-label="London Stage Database">
+    <nav>
         <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
             <button class="menu-icon" type="button" data-toggle="example-menu" aria-label="Open navigation menu"></button>
             <div class="title-bar-title">Menu</div>
         </div>
-        <div class="top-bar" role="banner">
+        <div class="top-bar" id="example-menu">
             <div class="top-bar-left">
-                <a role="link" href="/" class="menu-text">London Stage Database</a>
+                <a href="/" class="menu-text">London Stage Database</a>
             </div>
             <div class="top-bar-right">
-                <ul role="menu" class="dropdown menu" data-dropdown-menu>
-                    <li role="none">
-                        <a role="menuitem" href="/">Home</a>
-                    </li>
-                    <li role="none">
-                        <a role="menuitem" href="/search.php">Advanced Search</a>
-                    </li>
-                    <li role="none">
-                        <a role="menuitem" href="/data.php">Full Data Sets</a></li>
-                    <li role="none">
-                        <a role="menuitem" href="/guide.php">User Guide</a>
-                        <ul role="menu" class="menu vertical">
-                            <li role="none"><a role="menuitem" href="/about-data.php">About the Data</a></li>
-                            <li role="none"><a role="menuitem" href="/glossary.php">Glossary</a></li>
-                            <li role="none"><a role="menuitem" href="/cast-list.php">Cast Lists</a></li>
-                            <li role="none"><a role="menuitem" href="/authors.php">Authors</a></li>
-                            <li role="none"><a role="menuitem" href="/dates.php">Dates</a></li>
-                            <li role="none"><a role="menuitem" href="/tips.php">Search Tips</a></li>
+                <ul class="dropdown menu" data-dropdown-menu>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/search.php">Advanced Search</a></li>
+                    <li><a href="/data.php">Full Data Sets</a></li>
+                    <li>
+                        <a href="/guide.php">User Guide</a>
+                        <ul class="menu vertical">
+                            <li><a href="/about-data.php">About the Data</a></li>
+                            <li><a href="/glossary.php">Glossary</a></li>
+                            <li><a href="/cast-list.php">Cast Lists</a></li>
+                            <li><a href="/authors.php">Authors</a></li>
+                            <li><a href="/dates.php">Dates</a></li>
+                            <li><a href="/tips.php">Search Tips</a></li>
                         </ul>
                     </li>
                     <li>
                         <a href="/about.php">About</a>
-                        <ul role="menu" class="menu vertical">
-                            <li role="none"><a role="menuitem" href="/history.php">History</a></li>
-                            <li role="none"><a role="menuitem" href="/team.php">Team</a></li>
-                            <li role="none"><a role="menuitem" href="/citation.php">Citation and Sharing</a></li>
-                            <li role="none"><a role="menuitem" href="/media-coverage.php">Media Coverage</a></li>
+                        <ul class="menu vertical">
+                            <li><a href="/history.php">History</a></li>
+                            <li><a href="/team.php">Team</a></li>
+                            <li><a href="/citation.php">Citation and Sharing</a></li>
+                            <li><a href="/media-coverage.php">Media Coverage</a></li>
                         </ul>
                     </li>
-                    <li role="none"><a role="menuitem" href="/contact.php">Contact</a></li>
-                    <li role="none"><a role="menuitem" href="/news.php">News</a></li>
+                    <li><a href="/contact.php">Contact</a></li>
+                    <li><a href="/news.php">News</a></li>
                 </ul>
             </div>
 
