da1b8d22ff2e8779a906cee4a009519cc7b3eb6d
John <zzhao@uoregon.edu>
2026-04-24 10:16:24 -0700

    Removed redundent connection close codes out of downloading function because they would be auto-closed after php execution was done.

--- get_all_sphinx_json.php
+++ get_all_sphinx_json.php
@@ -62,8 +62,6 @@
     die();
   }
 
-  if (isset($conn) && $conn instanceof mysqli) { $conn->close(); }
-  if (isset($sphinx_conn) && $sphinx_conn instanceof mysqli) { $sphinx_conn->close(); }
 ?>
 <html>
 <head>
