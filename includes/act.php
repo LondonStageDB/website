8c798e033b21e8bcc9bdf5762f3629b07aba7a5f
Erin Winter <emwin@uoregon.edu>
2026-05-08 17:04:18 -0700

    revisd dropdowns pass 1

--- includes/act.php
+++ includes/act.php
@@ -1,22 +1,26 @@
 <?php
   include_once('functions.php');
-  global $conn;
+  global $sphinx_conn;
 
   $searchTerm = $_GET['term'];
   $searchTerm = cleanStr($searchTerm);
-  $searchTerm = mysqli_real_escape_string($conn, $searchTerm);
+  $searchTerm = mysqli_real_escape_string($sphinx_conn, $searchTerm);
 
   // If search string is over 3 chars, full wildcard search,
   // Else exact match beginning of string with wildcard at the end
   $search = strlen($searchTerm) > 3 ?
-    "SELECT *, (MATCH(PerformerClean) AGAINST ('\"$searchTerm\" @4' IN BOOLEAN MODE) + case when PerformerClean LIKE '$searchTerm' then 50 when PerformerClean LIKE '$searchTerm%' then 15 end) as relevance FROM `Cast` WHERE (MATCH(PerformerClean) AGAINST ('\"$searchTerm\" @4' IN BOOLEAN MODE) OR PerformerClean LIKE '%$searchTerm%') GROUP BY Performer ORDER BY relevance DESC, Performer LIMIT 10" :
-    "SELECT * FROM `Cast` WHERE PerformerClean LIKE '$searchTerm%' GROUP BY Performer ORDER BY Performer LIMIT 10";
+    "select performerclean from london_stages 
+                       where MATCH('@(performerclean,performer) $searchTerm*') group by performerclean 
+                    LIMIT 10":
+    "select performerclean from london_stages 
+                       where MATCH('@(performerclean,performer) *$searchTerm*') group by performerclean
+                    LIMIT 10";
 
-  $result = $conn->query($search);
+  $result = $sphinx_conn->query($search);
 
   $data = array();
   while ($row = $result->fetch_assoc()) {
-    $data[] = trim($row['PerformerClean']);
+    $data[] = trim($row['performerclean']);
   }
 
   echo json_encode(array_unique($data));
--- includes/perf.php
+++ includes/perf.php
@@ -1,23 +1,28 @@
 <?php
   include_once('functions.php');
-  global $conn;
+  global $sphinx_conn;
 
   $searchTerm = $_GET['term'];
   $searchTerm = cleanStr($searchTerm);
-  $searchTerm = mysqli_real_escape_string($conn, $searchTerm);
+  $searchTerm = mysqli_real_escape_string($sphinx_conn, $searchTerm);
 
   // If search string is over 3 chars, full wildcard search,
   // Else exact match beginning of string with wildcard at the end
-  $search = strlen($searchTerm) > 3 ?
-    "SELECT PerfTitleClean, (MATCH(PerfTitleClean) AGAINST ('\"$searchTerm\" @4' IN BOOLEAN MODE) + case when PerfTitleClean LIKE '$searchTerm' then 50 when PerfTitleClean LIKE '$searchTerm%' then 15 end) as relevance FROM `Performances` WHERE (MATCH(PerfTitleClean) AGAINST ('\"$searchTerm\" @4' IN BOOLEAN MODE) OR PerfTitleClean LIKE '%$searchTerm%') GROUP BY PerfTitleClean ORDER BY relevance DESC, PerfTitleClean LIMIT 10" :
-    "SELECT PerfTitleClean FROM `Performances` WHERE PerfTitleClean LIKE '$searchTerm%' GROUP BY PerfTitleClean ORDER BY PerfTitleClean LIMIT 10";
 
-  $result = $conn->query($search);
+  $search = strlen($searchTerm) > 3 ?
+      "select perftitleclean from london_stages 
+                       where MATCH('@(perftitleclean) $searchTerm*') group by perftitleclean 
+                    ORDER BY perftitleclean ASC LIMIT 10":
+      "select perftitleclean from london_stages 
+                       where MATCH('@(perftitleclean) *$searchTerm*') group by perftitleclean
+                    ORDER BY perftitleclean ASC LIMIT 10";
+
+  $result = $sphinx_conn->query($search);
 
   $data = array();
   if ($result) {
     while ($row = $result->fetch_assoc()) {
-      $data[] = trim($row['PerfTitleClean']);
+      $data[] = trim($row['perftitleclean']);
     }
   }
 
